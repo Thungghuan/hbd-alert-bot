f17cc839f799de0183e6e885a8bdf867044c146c
Thungghuan <1092887426@qq.com>
2021-08-18 17:46:41 +0800

    feat: update start message

--- index.ts
+++ index.ts
@@ -14,7 +14,21 @@ const token = process.env.BOT_TOKEN!
 
 const bot = new Telegraf(token)
 
-bot.start((ctx) => ctx.reply("Hello, I'm hbd_alert_bot"))
+bot.start((ctx) => {
+  let replyMSG =
+    "Hello, I'm HBD-alert-bot \n" +
+    `Chat ID here is ${ctx.chat.id} \n` +
+    `Chat type: ${ctx.chat.type} \n\n`
+
+  if (ctx.chat.type === 'private') {
+    const name = ctx.message.from.first_name + ctx.message.from?.last_name
+    replyMSG += `Hello, ${name}!`
+  } else {
+    replyMSG += 'Hello everyone.'
+  }
+
+  ctx.reply(replyMSG)
+})
 
 bot.help((ctx) => {
   ctx.reply("I'll show you whose birthday will come.")
