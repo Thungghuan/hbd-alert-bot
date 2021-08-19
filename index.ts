a6abfc92547ea26a0aa54e5615df0ac96b7c48f4
Thungghuan <1092887426@qq.com>
2021-08-19 18:44:29 +0800

    feat: add example codes about wizards

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Telegraf } from 'telegraf'
+import { Composer, Markup, Scenes, Telegraf } from 'telegraf'
 import express from 'express'
 import { DB } from './database'
 
@@ -13,13 +13,59 @@ app.listen(port)
 
 const token = process.env.BOT_TOKEN!
 
-const bot = new Telegraf(token)
+const bot = new Telegraf<Scenes.WizardContext>(token)
 
 const dbName = 'data.db'
+
+const stepHandler = new Composer<Scenes.WizardContext>()
+stepHandler.action('next', async (ctx) => {
+  await ctx.reply('Step 2. Via inline button')
+  return ctx.wizard.next()
+})
+stepHandler.command('next', async (ctx) => {
+  await ctx.reply('Step 2. Via command')
+  return ctx.wizard.next()
+})
+stepHandler.use((ctx) =>
+  ctx.replyWithMarkdown('Press `Next` button or type /next')
+)
+const superWizard = new Scenes.WizardScene(
+  'super-wizard',
+  async (ctx) => {
+    await ctx.reply(
+      'Step 1',
+      Markup.inlineKeyboard([
+        Markup.button.url('❤️', 'http://telegraf.js.org'),
+        Markup.button.callback('➡️ Next', 'next')
+      ])
+    )
+    return ctx.wizard.next()
+  },
+  stepHandler,
+  async (ctx) => {
+    await ctx.reply('Step 3')
+    return ctx.wizard.next()
+  },
+  async (ctx) => {
+    await ctx.reply('Step 4')
+    return ctx.wizard.next()
+  },
+  async (ctx) => {
+    await ctx.reply('Done')
+    return await ctx.scene.leave()
+  }
+)
+
+const stage = new Scenes.Stage<Scenes.WizardContext>([superWizard], {
+  default: 'super-wizard'
+})
+
+bot.use(stage.middleware())
 
 bot.start((ctx) => {
   let replyMSG =
     "Hello, I'm HBD-alert-bot \n" +
+    "I'll show you whose birthday will come." +
     `Chat ID here is ${ctx.chat.id} \n` +
     `Chat type: ${ctx.chat.type} \n\n`
 
@@ -40,9 +86,7 @@ bot.start((ctx) => {
   ctx.reply(replyMSG)
 })
 
-bot.help((ctx) => {
-  ctx.reply("I'll show you whose birthday will come.")
-})
+bot.help((ctx) => {})
 
 bot.command('hello', (ctx) => {
   const name = ctx.message.from.first_name + ctx.message.from?.last_name
