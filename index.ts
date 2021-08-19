cf5b6731f2ade5a191d2054c63a37608db837568
Thungghuan <1092887426@qq.com>
2021-08-19 21:48:41 +0800

    feat: add a test wizard

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { Composer, Markup, Scenes, Telegraf } from 'telegraf'
+import { Scenes, Telegraf } from 'telegraf'
 import express from 'express'
 import { DB } from './database'
+import { addDataWizard } from './addDataWizard'
 
 const app = express()
 
@@ -13,59 +14,14 @@ app.listen(port)
 
 const token = process.env.BOT_TOKEN!
 
-const bot = new Telegraf<Scenes.WizardContext>(token)
+const bot = new Telegraf(token)
 
 const dbName = 'data.db'
-
-const stepHandler = new Composer<Scenes.WizardContext>()
-stepHandler.action('next', async (ctx) => {
-  await ctx.reply('Step 2. Via inline button')
-  return ctx.wizard.next()
-})
-stepHandler.command('next', async (ctx) => {
-  await ctx.reply('Step 2. Via command')
-  return ctx.wizard.next()
-})
-stepHandler.use((ctx) =>
-  ctx.replyWithMarkdown('Press `Next` button or type /next')
-)
-const superWizard = new Scenes.WizardScene(
-  'super-wizard',
-  async (ctx) => {
-    await ctx.reply(
-      'Step 1',
-      Markup.inlineKeyboard([
-        Markup.button.url('❤️', 'http://telegraf.js.org'),
-        Markup.button.callback('➡️ Next', 'next')
-      ])
-    )
-    return ctx.wizard.next()
-  },
-  stepHandler,
-  async (ctx) => {
-    await ctx.reply('Step 3')
-    return ctx.wizard.next()
-  },
-  async (ctx) => {
-    await ctx.reply('Step 4')
-    return ctx.wizard.next()
-  },
-  async (ctx) => {
-    await ctx.reply('Done')
-    return await ctx.scene.leave()
-  }
-)
-
-const stage = new Scenes.Stage<Scenes.WizardContext>([superWizard], {
-  default: 'super-wizard'
-})
-
-bot.use(stage.middleware())
 
 bot.start((ctx) => {
   let replyMSG =
     "Hello, I'm HBD-alert-bot \n" +
-    "I'll show you whose birthday will come." +
+    "I'll show you whose birthday will come.\n" +
     `Chat ID here is ${ctx.chat.id} \n` +
     `Chat type: ${ctx.chat.type} \n\n`
 
@@ -87,6 +43,10 @@ bot.start((ctx) => {
 })
 
 bot.help((ctx) => {})
+
+bot.hears('add_data', (ctx) => {
+  ;(ctx as any).scene.enter('add_data_wizard')
+})
 
 bot.command('hello', (ctx) => {
   const name = ctx.message.from.first_name + ctx.message.from?.last_name
