59028fd74a8350502e1279ad7f28a4606a2195a5
Thungghuan <1092887426@qq.com>
2021-08-20 19:40:08 +0800

    feat: add text process when adding data

--- scenes/addDataWizard.ts
+++ scenes/addDataWizard.ts
@@ -8,18 +8,22 @@ interface BirthdayData {
 
 export const addDataWizard = new Scenes.WizardScene(
   'add_data',
-  async (ctx: any) => {
+  (ctx: any) => {
     const replyMSG =
-      '🥳 Send the birthday day list please.\n' +
-      '📝 Format: NAME MM-DD,\n' +
-      '     separated by a space or a newline\n'
-    await ctx.reply(replyMSG)
+      '🥳 Send the birthday day list please\\.\n' +
+      '📝 Format: NAME MM\\.DD\n' +
+      '_Separated by Pattern: _\n' +
+      '`/:|：|s+|\\n|s*周[日一二三四五六]/`\n' +
+      '_人话: colon, space, newline, 周x_ \n'
+    ctx.replyWithMarkdownV2(replyMSG)
     return ctx.wizard.next()
   },
   async (ctx) => {
-    const textSplit: string[] = ctx.message.text.split(/[\s\n]/)
-    const names = textSplit.filter((el, i) => i % 2 === 0)
-    const dates = textSplit.filter((el, i) => i % 2 === 1)
+    const textSplit: string[] = ctx.message.text.split(
+      /:|：|s+|\n|s*周[日一二三四五六]/
+    )
+    const names = textSplit.filter((el, i) => el && i % 2 === 0)
+    const dates = textSplit.filter((el, i) => el && i % 2 === 1)
 
     const birthdayData: BirthdayData[] = []
     names.forEach((name, i) => {
